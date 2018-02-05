8fc8653ab3ee7ebfa12533afffc86e40e1ef8f9b
Xin Wang <wangxin00163@yahoo.co.uk>
2018-02-05 18:10:45 +0000

    Say number when it's not divisible by 3, 5 or 15

--- FizzBuzz/Brain.swift
+++ FizzBuzz/Brain.swift
@@ -11,11 +11,21 @@ class Brain {
     func isDivisbleBy(divisor: Int, number: Int) -> Bool {
         return number % divisor == 0
     }
+    func check(number: Int) -> String {
+        if isDivisibleByFifteen(number: number) {
+            return "FizzBuzz"
+        } else if isDivisibleByFive(number: number){
+            return "Buzz"
+        } else if isDivisibleByFive(number: number){
+            return "Fizz"
+        } else {
+            return "\(number)"
+        }
+    }
     
     func isDivisibleByThree(number: Int) -> Bool {
         return isDivisbleBy(divisor: 3, number: number)
     }
-    
     func isDivisibleByFive(number: Int) -> Bool {
         return isDivisbleBy(divisor: 5, number: number)
     }
