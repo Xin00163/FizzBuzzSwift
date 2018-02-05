7ee5ccca6c619a48aac874d01a250784500f6315
Xin Wang <wangxin00163@yahoo.co.uk>
2018-02-05 17:41:45 +0000

    Test pass for 3, 5, and 15

--- FizzBuzzTests/BrianTests.swift
+++ FizzBuzzTests/BrianTests.swift
@@ -10,6 +10,7 @@ import XCTest
 @testable import FizzBuzz
 
 class BrianTests: XCTestCase {
+    let brain = Brain()
     
     override func setUp() {
         super.setUp()
@@ -22,9 +23,32 @@ class BrianTests: XCTestCase {
     }
     
     func testisDivisibleByThree() {
-        let brain = Brain()
         let result = brain.isDivisibleByThree(number: 3)
         XCTAssertEqual(result, true)
     }
     
+    func testisNotDivisibleByThree() {
+        let result = brain.isDivisibleByThree(number: 5)
+        XCTAssertEqual(result, false)
+    }
+    
+    func testisDivisibleByFive() {
+        let result = brain.isDivisibleByFive(number: 5)
+        XCTAssertEqual(result, true)
+    }
+    
+    func testisNotDivisibleByFive() {
+        let result = brain.isDivisibleByThree(number: 7)
+        XCTAssertEqual(result, false)
+    }
+    
+    func testisDivisibleByFifteen() {
+        let result = brain.isDivisibleByFifteen(number: 15)
+        XCTAssertEqual(result, true)
+    }
+    
+    func testisNotDivisibleByFifteen() {
+        let result = brain.isDivisibleByFifteen(number: 1)
+        XCTAssertEqual(result, false)
+    }
 }
